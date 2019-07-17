a2a0dcfc16245142c83e9fd878bb8c0ca0b31660
moondewwishes <nwahal@ucsd.edu>
2019-07-17 14:06:27 -0700

    allow easy change of segment length

--- WAVsplit.py
+++ WAVsplit.py
@@ -8,6 +8,7 @@ Created on Wed Jul 17 13:54:59 2019
 from pydub import AudioSegment
 import os
 
+segLen = 5
 directory = './MoonSplit/'
 
 orig = AudioSegment.from_file('./exports/MoonCombo.wav', format='wav')
@@ -15,6 +16,6 @@ orig = AudioSegment.from_file('./exports/MoonCombo.wav', format='wav')
 if not os.path.exists(directory):
     os.makedirs(directory)
     
-for i, chunk in enumerate(orig[::5000]):
+for i, chunk in enumerate(orig[::(segLen * 1000)]):
     with open('%sMoon5SecSeg-%s.wav'%(directory, i+1), 'wb') as f:
         chunk.export(f, format='wav')
