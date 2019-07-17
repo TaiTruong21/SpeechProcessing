9af5edeeae9d927a6861462a45ce212ed287c44e
moondewwishes <nwahal@ucsd.edu>
2019-07-17 15:55:26 -0700

    start tuning parameters based off speechandnoisecombo

--- speechactivitydetector.py
+++ speechactivitydetector.py
@@ -12,12 +12,12 @@ import scipy.fftpack
 import scipy.io
 from scipy.io import wavfile as wf
 
-Fs, data = wf.read("tester10syl.wav")
+Fs, data = wf.read("./exports/SpeechAndNoiseCombo.wav")
 T = 1/Fs
-frameSize = 0.7
-eThresh = 40
-fThresh = 185
-sfmThresh = 5   
+frameSize = 0.5
+eThresh = 20000*100000
+fThresh = 195
+sfmThresh = 2000
 fftN = 20000
 freqs = np.fft.fftfreq(fftN, T)
 
@@ -44,6 +44,7 @@ print("data length = ",len(data))
 
 
 for i in range(numFrames):
+    
     #finds the overall properties of input frame
     frame = data[(i*samplesPerFrame):(i*samplesPerFrame + samplesPerFrame)]
     fftFrame = scipy.fftpack.fft(frame, fftN)
@@ -62,21 +63,23 @@ for i in range(numFrames):
     domFreq = freqs[maxindex]
     energy = np.sum(pwr)/len(pwr)
     SFM = mag[int(samplesPerFrame/2)]
-    counter = 0
+    #print('%.2f'%SFM)
     time = i*samplesPerFrame / Fs
     isSpeech = False
-    
+    counter = 0    
     #updates counter to determine if frame is speech
     if (domFreq > fThresh): counter+=1
     if (energy > eThresh): counter+=1
     if (SFM > sfmThresh): counter+=1
     if (counter > 1):
         isSpeech = True
-        allClass.append([1, time])
+        allClass.append([1, time, domFreq, energy/100000, SFM])
     else:
         isSpeecch = False
-        allClass.append([0, time])
+        allClass.append([0, time, domFreq, energy/100000, SFM])
 
     #print('Time: ', time, '\n', isSpeech, "\n Energy = ", energy, "\n domFreq = ", domFreq, "\n SFM = ", SFM)
-
+np.set_printoptions(precision=3)
+np.set_printoptions(suppress=True)
 print(np.matrix(allClass))
+np.savetxt('results.txt', np.matrix(allClass), fmt='%.2f')
