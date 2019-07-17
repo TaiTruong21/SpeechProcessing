2b66d0489486e32433883106423f0ddb6b63ec7c
moondewwishes <nwahal@ucsd.edu>
2019-07-17 14:03:34 -0700

    split moon combo into 5 sec segments

---
+++ WAVsplit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 13:54:59 2019
+
+@author: nihar
+"""
+
+from pydub import AudioSegment
+import os
+
+directory = './MoonSplit/'
+
+orig = AudioSegment.from_file('./exports/MoonCombo.wav', format='wav')
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    
+for i, chunk in enumerate(orig[::5000]):
+    with open('%sMoonSeg-%s.wav'%(directory, i), 'wb') as f:
+        chunk.export(f, format='wav')
