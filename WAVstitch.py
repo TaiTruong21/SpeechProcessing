4019dadc9ecf88924639101a06f92cc8dc5c2ccc
moondewwishes <nwahal@ucsd.edu>
2019-07-16 17:12:40 -0700

    completed wavstitch code

--- WAVstitch.py
+++ WAVstitch.py
@@ -7,4 +7,20 @@ README:
     asldfha
 """
 
-import pydub
+import os
+from pydub import AudioSegment
+
+combinationName = 'test'
+directory = './exports/'
+
+combined = AudioSegment.empty()
+
+for file in os.listdir('./keyboard only/'):
+    if file.endswith('.wav'):
+        print(file)
+        add = AudioSegment.from_file('./keyboard only/'+file)
+        combined += add
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+combined.export('./exports/%s.wav'%(combinationName), format = 'wav')
